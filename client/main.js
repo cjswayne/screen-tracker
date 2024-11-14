394cc91a4532a9735d815eefba4d4bc7e0020fa0
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-11-14 14:24:37 -0500

    commit

--- client/main.js
+++ client/main.js
@@ -1,5 +1,10 @@
-import  { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __filename and __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Enable live reload for Electron
 import electronReload from 'electron-reload';
